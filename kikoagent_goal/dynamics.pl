3d47349ff0c19ff0f848ec7130503945ba51d609
karinemiras <karine.smiras@gmail.com>
2020-05-19 23:13:21 +0200

    watching subjects

--- kikoagent_goal/dynamics.pl
+++ kikoagent_goal/dynamics.pl
@@ -1,2 +1,4 @@
- :-dynamic helping/0,  % his main and constant goal
- 	   event/1.    % nao events, i.e., say, gesture, touch etc.
+ :-dynamic helping/0,  			% his main and constant goal
+ 	   event/1,    			% nao events, i.e., say, gesture, touch etc.
+ 	   personDetected/0, 		% a person has been detected by the startWathing action
+ 	   watching/0.  		% the startWathing action is taking place	  
