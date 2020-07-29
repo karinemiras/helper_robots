2d893e612c520507b2428741c81ce6a00fe7c131
karinemiras <karine.smiras@gmail.com>
2020-07-29 18:23:15 +0200

    find employee query

--- kikoagent_Xplain/find_employee.py
+++ kikoagent_Xplain/find_employee.py
@@ -12,21 +12,40 @@ class FindEmployee:
 
         self.agent.say_and_wait(belief_type='employee_name',
                                 say_text=self.agent.get_sentence('find_employee', 'ask_name'),
-                                unexpected_answer_params=[self.agent.xplain.belief_params('speech_text')],
+                                unexpected_answer_topic='find_employee',
                                 timeout=self.agent.parameters['timeout_listening'])
 
         if self.agent.xplain.is_belief('employee_name'):
+           
+            similarity_threshold = 0.4
+            try:
+                cursor = self.agent.postgres.connection.cursor()
+                query = "select * from (SELECT *, similarity(replace(name,' ',''), replace(%s,' ','')) AS sim " \
+                        "FROM employees order by sim desc limit 1 ) as f  where sim >= %s"
+                cursor.execute(query, (self.agent.xplain.belief_params('employee_name'), similarity_threshold))
+                records = cursor.fetchall()
+                cursor.close()
 
-            if self.agent.xplain.belief_params('employee_name') == 'Charlie Brown':
-                self.agent.say('Charlie Brown works on the third floor, T345.')
-            if self.agent.xplain.belief_params('employee_name') == 'Bob Dylan':
-                self.agent.say('Bob Dylan works on the second floor, T240.')
-            if self.agent.xplain.belief_params('employee_name') == '':
-                self.agent.say('I could not find '+self.agent.xplain.belief_params('speech_text'))
+            except Exception as error:
+                self.agent.log.write('\nERROR db find employee: {}'.format(error))
 
-            self.agent.xplain.drop('employee_name')
-            self.agent.clear_answer_beliefs()
-            self.agent.xplain.drop('helping')
+            if len(records) > 0:
+
+                self.agent.say('I found this person!')
+                # give and show info
+
+                self.agent.xplain.drop('employee_name')
+                self.agent.clear_answer_beliefs()
+                self.agent.xplain.drop('helping')
+
+            else:
+                self.agent.say(self.agent.get_sentence('find_employee', 'not_found',
+                                                       [self.agent.xplain.belief_params('employee_name')]))
+                self.agent.xplain.drop('employee_name')
+                self.agent.xplain.drop('speech_text')
+                self.agent.xplain.adopt('input.unknown', 'cognition')
+                self.agent.xplain.adopt('waiting_answer', 'action')
+
 
 
 
